c9cbacdf2ec06e3df863ca06d4c61500e9e65af4
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-05-22 23:47:33 +0530

    Subtraction works with 3 digit numbers onyl for now

--- Experiment 7/sub.asm
+++ Experiment 7/sub.asm
@@ -6,33 +6,39 @@
     int 80h
 %endmacro
 
-%macro read 1
+%macro read 2
     mov eax, 3
     mov ebx, 2
     mov ecx, %1 
-    mov edx, 9
+    mov edx, %2
     int 80h
 %endmacro
 
 section .data
-    ask db "Enter a number: "
-    asklen equ $-ask
+    askmin db "Enter the minuend: "
+    askminlen equ $-askmin
+
+    asksub db "Enter the subtrahend: "
+    asksublen equ $-asksub
 
     show db "The Diff: "
     showlen equ $-show
+
+    nl db '', 10
+    nllen equ $-nl
 section .bss
-    num1 resb 9
-    num2 resb 9
-    diff resb 9
+    num1 resb 5
+    num2 resb 5
+    diff resb 5
 
 section .text
     global _start
 
 _start:
-    write ask, asklen
-
-    read num1
-    read num2 
+    write askmin, askminlen
+    read num1, 5
+    write asksub, asksublen
+    read num2, 5
 
     mov esi, 2
     mov ecx, 3
@@ -40,19 +46,23 @@ _start:
 
     sub_loop:
 
-    mov al, [n1 + esi]
-    sbb al, [n1 + esi]
+    mov al, [num1 + esi]
+    sbb al, [num2 + esi]
     aas
     pushf
     or al, 30h
     popf
 
+    ; or al, 30h
     mov [diff + esi], al
+    
     dec esi
     loop sub_loop
 
     write show, showlen
     write diff, 5
+
+    write nl, nllen
 
     mov eax, 1
     int 80h
