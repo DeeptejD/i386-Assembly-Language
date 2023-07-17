1ac52e43294fd75ba5845e5d1e750c4b66d9e761
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-07-17 13:31:05 +0530

    Added a revised linear search code

--- Archive/Miscellaneous/practical_exam/lineartest.asm
+++ Archive/Miscellaneous/practical_exam/lineartest.asm
@@ -2,7 +2,28 @@ section .data
     nl db "", 10
     nllen equ $-nl 
 
-    
+    asksize db 'Entert the number of elements: '
+    asksizelen equ $-asksize
+
+    ask db 'Enter the elements'
+    asklen equ $-ask
+
+    space db ' '
+    spacelen equ $-space
+
+    showarray db 'Array: '
+    showarraylen equ $-showarray
+
+    aski db 'Enter the element to be searched: '
+    askilen equ $-aski
+
+    f db 'Element found at index: '
+    flen equ $-f
+
+    nf db 'Element not found'
+    nflen equ $-nf
+
+    array times 10 dw 0
 
 
 ; WRITE MACRO
@@ -35,16 +56,115 @@ section .data
 
 ; DECLARE VARIABLES
 section .bss
-    
+    num resb 1
+    element resb 1
+    counter resb 1
+    temp resb 1
+    pos resb 1
 
 
 section .text
     global _start
 _start:
     
+    ; ask size
+    write asksize, asksizelen
+    read num, 2
+
+    write ask, asklen
+    endl
+
+    ; input loop
+    mov byte[counter], 0
+    mov esi, array
+
+    input:
+        read temp, 2
+        mov al, [temp]
+        mov [esi], al
+
+        inc esi
+        inc byte[counter]
+
+        mov al, [counter]
+        mov bl, [num]
+
+        sub bl, '0'
+
+        cmp al, bl
+
+        je ask_element
+        jmp input
+
+    ask_element:
+        write aski, askilen
+        read element, 2
     
+    display:
+        write showarray, showarraylen
+        mov esi, array
+        mov byte[counter], 0
+
+        displ_loop:
+            write esi, 1
+            write space, spacelen
+
+            inc esi
+            inc byte[counter]
+
+            mov al, [counter]
+            mov bl, [num]
+            sub bl, '0'
+
+            cmp al, bl
+            je check
+            jmp displ_loop
+    
+    check:
+        endl
+        mov esi, array
+        mov byte[counter], 0
+
+        check_loop:
+            mov al, [esi]
+            mov bl, [element]
+            sub al, '0'
+            sub bl, '0'
+            cmp al, bl
+            je found
+            jmp notfound
+        
+    found:
+        write f, flen
+        add bl, '0'
+        mov al, [counter]
+        add al, '0'
+        mov [counter], al
+        write counter, 1
+        endl
+        jmp exit
+
+    notfound:
+        inc esi
+        inc byte[counter]
+
+        mov al, [num]
+        sub al, '0'
+        mov bl, [counter]
+
+        cmp al, bl
+
+        je failure
+        jmp check_loop
+    
+    failure:
+        write nf, nflen
+        endl
+        jmp exit
+
 
 ; EXIT CALL
+    exit:
     mov eax, 1
     mov ebx, 0
     int 80h
